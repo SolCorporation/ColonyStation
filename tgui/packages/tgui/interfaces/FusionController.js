dcded1c86b2d060e9caf6d26c8ce838fbbcc4cea
TheGamerdk <5618080+TheGamerdk@users.noreply.github.com>
2020-10-27 16:23:53 +0100

    compile

--- tgui/packages/tgui/interfaces/FusionController.js
+++ tgui/packages/tgui/interfaces/FusionController.js
@@ -11,55 +11,56 @@ export const FusionController = (props, context) => {
     <Window resizable>
       <Window.Content scrollable>
         <Section title="Reactor Monitoring">
-        <LabeledList>
-        <LabeledList.Item label="Internal Heat">
-          {data.internal_heat}K
-        </LabeledList.Item>
-          <LabeledList.Item label="Max Internal Heat">
-            {data.internal_heat_max}K
-          </LabeledList.Item>
-          <LabeledList.Divider />
-          <LabeledList.Item label="Containment Heat">
-            {data.core_heat}K
-          </LabeledList.Item>
-          <LabeledList.Item label="Max Containment Heat">
-            {data.core_heat_max}K
-          </LabeledList.Item>
-          <LabeledList.Item label="Containment Health">
-            {data.health}%
-          </LabeledList.Item>
-          <LabeledList.Divider />
-          <LabeledList.Item label="Fuel Use">
-            {data.fuel_use} moles per rod, per activation.
-          </LabeledList.Item>
-          <LabeledList.Item label="Fuel Rods">
-            {data.fuel && (
-              <Fragment>
-              {map((value, key) => (
-                <Section>
-                  <LabeledList>
-                  <LabeledList.Item label="Fuel Type">
-                    {value.name}
-                  </LabeledList.Item>
-                  <LabeledList.Item label="Fuel Amount">
-                    {value.amount} moles ({Math.round((value.amount / value.max_amount) * 100)}%)
-                  </LabeledList.Item>
-                  <LabeledList.Item label="Fuel Power Multiplier">
-                    {value.power_multi}x per activation.
-                  </LabeledList.Item>
-                  <LabeledList.Item label="Fuel Heat Multiplier">
-                    {value.heat_multi}x per activation.
-                  </LabeledList.Item>
-                  </LabeledList>
-                </Section>
+          <LabeledList>
+            <LabeledList.Item label="Internal Heat">
+              {data.internal_heat}K
+            </LabeledList.Item>
+            <LabeledList.Item label="Max Internal Heat">
+              {data.internal_heat_max}K
+            </LabeledList.Item>
+            <LabeledList.Divider />
+            <LabeledList.Item label="Containment Heat">
+              {data.core_heat}K
+            </LabeledList.Item>
+            <LabeledList.Item label="Max Containment Heat">
+              {data.core_heat_max}K
+            </LabeledList.Item>
+            <LabeledList.Item label="Containment Health">
+              {data.health}%
+            </LabeledList.Item>
+            <LabeledList.Divider />
+            <LabeledList.Item label="Fuel Use">
+              {data.fuel_use} moles per rod, per activation.
+            </LabeledList.Item>
+            <LabeledList.Item label="Fuel Rods">
+              {data.fuel && (
+                <Box>
+                  {map((value, key) => (
+                    <Section>
+                      <LabeledList>
+                        <LabeledList.Item label="Fuel Type">
+                          {value.name}
+                        </LabeledList.Item>
+                        <LabeledList.Item label="Fuel Amount">
+                          {value.amount} moles
+                          ({Math.round((value.amount / value.max_amount) * 100)}%)
+                        </LabeledList.Item>
+                        <LabeledList.Item label="Fuel Power Multiplier">
+                          {value.power_multi}x per activation.
+                        </LabeledList.Item>
+                        <LabeledList.Item label="Fuel Heat Multiplier">
+                          {value.heat_multi}x per activation.
+                        </LabeledList.Item>
+                      </LabeledList>
+                    </Section>
 
-              ))(data.fuel)}
-              </Fragment>
-            ) || "None"}
+                  ))(data.fuel)}
+                </Box>
+              ) || "None"}
 
-          </LabeledList.Item>
+            </LabeledList.Item>
 
-        </LabeledList>
+          </LabeledList>
         </Section>
       </Window.Content>
     </Window>
